a56b703c17fdfbb9dce68c72f33570275c3a2183
Vaughub <olakamfjord@gmail.com>
2020-12-04 15:37:32 +0100

    day04

---
+++ Dag-03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+
+namespace Dag_03
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var requestM = new RestRequest("d277d4f01a9fe10f7c1d92e2d17f1b31/raw/49da54e4372a83f4fc11d7137f19fc8b4c58bda6/matrix.txt");
+			var matrisa = new RestClient("https://gist.githubusercontent.com/knowitkodekalender/").Execute(requestM).Content;
+
+			var request = new RestRequest("9e1ba20cd879b0c6d7af4ccfe8a87a19/raw/b19ae9548a33a825e2275d0283986070b9b7a126/wordlist.txt");
+			var wordList = new RestClient("https://gist.githubusercontent.com/knowitkodekalender/").Execute(request).Content.Split('\n').ToArray();
+
+
+			foreach (var word in wordList)
+			{
+				if(matrisa.Contains(word)) Console.WriteLine(word);
+			}
+
+
+			var reverseWordList = wordList.Select(s => s.ToCharArray().Reverse().ToArray()).Select(word => new string(word)).ToList();
+
+
+			foreach (var word in reverseWordList.Where(word => matrisa.Contains(word)))
+			{
+				Console.WriteLine(new string(word.ToCharArray().Reverse().ToArray()));
+			}
+
+
+			ReverseString(wordList);
+
+
+			Console.WriteLine();
+		}
+
+		static List<string> ReverseString(IEnumerable<string> stg)
+		{
+			return stg.Select(s => s.ToCharArray().Reverse().ToArray()).Select(word => new string(word)).ToList();
+		}
+	}
+}
---
+++ Dag-04/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using RestSharp;
+
+namespace Dag_04
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var requestM = new RestRequest("leveringsliste.txt");
+			var response = new RestClient("https://julekalender-backend.knowit.no/challenges/4/attachments/").Execute(requestM).Content;
+
+			var ingredient = response.Split('\n')
+				.Select(s => s.Split(", ").Select(s2 => s2.Split(": "))
+					.Select(s3 => new { Ingredient = s3.First(), Quantity = int.Parse(s3.Last()) }))
+				.SelectMany(l => l)
+				.Aggregate(new { Sugar = 0, Milk = 0, Egg = 0, Flour = 0 }, (total, next) => new
+				{
+					Sugar = next.Ingredient == "sukker" ? total.Sugar + next.Quantity : total.Sugar,
+					Milk = next.Ingredient == "melk" ? total.Milk + next.Quantity : total.Milk,
+					Egg = next.Ingredient == "egg" ? total.Egg + next.Quantity : total.Egg,
+					Flour = next.Ingredient == "mel" ? total.Flour + next.Quantity : total.Flour
+				});
+
+			var cakeBaked = Lowest(ingredient.Sugar / 2, ingredient.Milk / 3, ingredient.Flour / 3, ingredient.Egg);
+
+			Console.WriteLine($"Number of cake baked is {cakeBaked}");
+		}
+
+		public static int Lowest(params int[] inputs)
+		{
+			return inputs.Min();
+		}
+	}
+}
+
+
+//var strings = response.Split('\n');
+
+//var sugar = 0;
+//var flour = 0;
+//var milk = 0;
+//var egg = 0;
+
+//foreach (var t1 in strings)
+//{
+//	var line = t1.Split(',');
+
+//	foreach (var t in line)
+//	{
+//		if (t.Contains("melk")) milk += int.Parse(t.Split(':')[1]);
+//		else if (t.Contains("egg")) egg += int.Parse(t.Split(':')[1]);
+//		else if (t.Contains("mel")) flour += int.Parse(t.Split(':')[1]);
+//		else if (t.Contains("sukker")) sugar += int.Parse(t.Split(':')[1]);
+//		else throw new Exception("Error");
+//	}
+//}
+
+//sugar /= 2;
+//flour /= 3;
+//milk /= 3;
+
+//var cakeBaked = Lowest(sugar, flour, milk, egg);
+
+//Console.WriteLine($"Number of cake baked is {cakeBaked}");
