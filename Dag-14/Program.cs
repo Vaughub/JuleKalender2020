fef097f3879b96723917a81ac0cf88066039b351
Vaughub <olakamfjord@gmail.com>
2020-12-19 11:12:38 +0100

    .

---
+++ Dag-14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dag_14
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sequence = new List<int> {0, 1};
+
+			for (int i = 2; i < 15; i++)
+			{
+				if (sequence[i - 2] - i > 0 && !sequence.Contains(sequence[i - 2] - i)) sequence.Add(sequence[i - 2] - i);
+				if (sequence[i - 2] + i > 0 && !sequence.Contains(sequence[i - 2] + i)) sequence.Add(sequence[i - 2] + i);
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
